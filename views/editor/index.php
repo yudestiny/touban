8f7fa38ea92ebf099c80b2557da54c9cd1a0780a
yudestiny <yudestiny.program@gmail.com>
2024-04-07 18:18:29 +0900

    編集画面にて最低当番回数とタイプのインプット欄を追加し、初期値をデータベースから取得した値に設定しました

--- views/editor/index.php
+++ views/editor/index.php
@@ -1,7 +1,7 @@
 <h2>社員の編集</h2>
 <div>
-  <p><? echo $current['name']; ?>さん、こんにちは </p>
-  <p>現在の当番最大回数は<? echo $current['limit']; ?>回です</p>
+  <p><?= $current['name'] ?>さん、こんにちは </p>
+  <p>現在の当番最大回数は<?= $current['maxLimit']; ?>回、当番最小回数は<?= $current['minLimit']; ?>回です</p>
 </div>
 <?php if (empty($_POST['btn_submit']) && !empty($_SESSION['success_message'])) : ?>
   <p class="success_message"><?php echo htmlspecialchars($_SESSION['success_message'], ENT_QUOTES, 'UTF-8'); ?></p>
@@ -15,9 +15,20 @@
 <div>
   <form action="./editor?id=<?= $current['id'] ?>" method='POST'>
     <label for="name">名前</label>
-    <input type="text" id="name" name="name">
-    <label for="limit">当番最大回数</label>
-    <input type="text" id="limit" name="limit">
+    <input type="text" id="name" name="name" value="<?= $current['name'] ?>">
+    <select name="type" >
+      <?php foreach($types as $type) : ?>
+    <option value="<?= ($type['id']) ?>"
+      <?php if($type['id'] === $current['type']) : ?>
+        selected
+      <?php endif ?>
+    ><?= ($type['name']) ?></option>
+      <?php endforeach ?>
+    </select>
+    <label for="maxLimit">当番最大回数</label>
+    <input type="text" id="maxLimit" name="maxLimit" value="<?= $current['maxLimit'] ?>">
+    <label for="minLimit">当番最小回数</label>
+    <input type="text" id="minLimit" name="minLimit" value="<?= $current['minLimit'] ?>">
     <input type="submit" value="変更する">
   </form>
 </div>
